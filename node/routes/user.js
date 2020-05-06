5550343290af2fd8742bb862110c8279e6ef43d7
longyzw <752490640@qq.com>
2020-05-06 17:27:42 +0800

    Update user.js

--- node/routes/user.js
+++ node/routes/user.js
@@ -36,7 +36,6 @@ router.get('/list', async (ctx, next) => {
 // 用户详情
 router.get('/detail', async (ctx, next) => {
     const userInfo = await detail(ctx.state)
-	// console.log("info", userInfo)
     ctx.body = new SuccessModel('0000', userInfo)
 });
 
